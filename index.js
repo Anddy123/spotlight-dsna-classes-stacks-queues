d5f7f9030dd2a9d751e8a9537587e3e38a2f6d9e
Anddy <alonzoa2015@gmail.com>
2022-06-08 15:23:09 -0700

    peek test finished

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-class Stack {}
+class Stack {
+    #list = [];
+
+  constructor(initialValue) {
+    if (initialValue) {
+      this.#list = initialValue;
+    }
+  }
+
+  push(item) {
+    this.#list.push(item);
+  }
+
+  peek() {
+    return this.#list[this.#list.length - 1];
+  }
+}
 
 class Queue {}
 
